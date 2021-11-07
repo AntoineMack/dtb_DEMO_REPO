3c9590b662454e256aa4207ff75cd9992e2a0098
Antoine Mack <awmack22@gmail.com>
2021-11-07 23:16:20 +0000

    Data Source Clean up - change data types, naming conv, soft deletes...

--- models/staging/stg_product.sql
+++ models/staging/stg_product.sql
@@ -1,4 +1,8 @@
 with product as ( select * from {{ source('subscription', 'product') }} )
 
-select *
+select Id as id, name, unit,
+    CAST(replace(unit_rate,',','') as number) as unit_rate, type,
+    unit_timing, 
+    CASE WHEN status = 'Activ' THEN 'Active' ELSE status END as status
 from product
+where deleted_at is NULL and status != 'Archived'
